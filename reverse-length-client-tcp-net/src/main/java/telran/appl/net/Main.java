b101bdd3325a41e17fab68bb859cb6b75f4236e3
MariaGins <m.gins@mail.ru>
2024-11-10 20:44:01 +0200

    Added switch case

--- reverse-length-client-tcp-net/src/main/java/telran/appl/net/Main.java
+++ reverse-length-client-tcp-net/src/main/java/telran/appl/net/Main.java
@@ -1,49 +1,54 @@
 package telran.appl.net;
-
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.List;
-import org.json.JSONObject;
-import telran.view.*;
 
+
+import telran.net.TcpClient;
+import telran.view.*;
 public class Main {
-    static ReverseLengthClient client;
+    static TcpClient client;
 
     public static void main(String[] args) {
-        Item[] items = {
+      Item[] items = {
             Item.of("start session", Main::startSession),
             Item.of("exit", Main::exit, true)
-        };
-        Menu menu = new Menu("Echo Application", items);
-        menu.perform(new StandardInputOutput());
+      };
+      Menu menu = new Menu("Echo Application", items);
+      menu.perform(new StandardInputOutput());
     }
-
     static void startSession(InputOutput io) {
         String host = io.readString("Enter hostname");
         int port = io.readNumberRange("Enter port", "Wrong port", 3000, 50000).intValue();
-        if (client != null) {
-            client.close();
+        if(client != null) {
+            try {
+                client.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        client = new ReverseLengthClient(host, port);
+        client = new TcpClient(host, port);
         Menu menu = new Menu("Run Session",
-            Item.of("session", Main::sessionProcessing),
-            Item.ofExit());
-        menu.perform(io);
-    }
+         Item.of("session", Main::sessionProcessing),
+          Item.ofExit());
+         menu.perform(io);
 
+    }
     static void sessionProcessing(InputOutput io) {
-        HashSet<String> types = new HashSet<>(List.of("reverse", "length"));
+       HashSet<String> types = new HashSet<>(List.of("reverse", "length", "wrong_type_test"));
         String type = io.readStringOptions("Enter operation type " + types, "Wrong type", types);
         String string = io.readString("Enter any string");
-        JSONObject jsonObj = new JSONObject();
-        jsonObj.put("requestType", type);
-        jsonObj.put("requestData", string);
-        String response = client.sendAndReceive(jsonObj.toString());
+       
+        String response = client.sendAndReceive(type, string);
         io.writeLine(response);
     }
-
     static void exit(InputOutput io) {
-        if (client != null) {
-            client.close();
+        if(client != null) {
+            try {
+                client.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- reverse-length-server-tcp-net/src/main/java/telran/appl/net/ReverseLengthProtocol.java
+++ reverse-length-server-tcp-net/src/main/java/telran/appl/net/ReverseLengthProtocol.java
@@ -6,21 +6,38 @@ import telran.net.Response;
 import telran.net.ResponseCode;
 
 public class ReverseLengthProtocol implements Protocol {
+
     @Override
     public Response getResponse(Request request) {
-        String requestType = request.requestType();
-        String requestData = request.requestData();
-        String result = "";
-
-        switch (requestType) {
-            case "reverse":
-                result = new StringBuilder(requestData).reverse().toString();
-                return new Response(ResponseCode.OK, result);
-            case "length":
-                result = String.valueOf(requestData.length());
-                return new Response(ResponseCode.OK, result);
-            default:
-                return new Response(ResponseCode.WRONG_TYPE, "Unsupported request type: " + requestType);
+        String type = request.requestType();
+        String data = request.requestData();
+        Response response = null;
+        try {
+            response = switch (type) {
+                case "reverse" -> reverse(data);
+                case "length" -> length(data);
+                default -> new Response(ResponseCode.WRONG_TYPE, type + " is wrong type");
+            };
+        } catch (Exception e) {
+           response = new Response(ResponseCode.WRONG_DATA, e.getMessage());
         }
-    }   
+        return response;
+    }
+
+    Response reverse(String data) {
+        if(data.length() < 2) {
+            throw new IllegalArgumentException("too few string length: must be greater than 1");
+        }
+        String res = new StringBuilder(data).reverse().toString();
+        return getOkResponse(res);
+    }
+    Response length(String data) {
+        String res = data.length() + "";
+        return getOkResponse(res);
+    }
+
+    private Response getOkResponse(String res) {
+       return new Response(ResponseCode.OK, res);
+    }
+
 }
